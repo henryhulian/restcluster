5f7f1e8edd793b12a2b328920f122c12e36aa0b1
fb421421 <fb421421@gmail.com>
2014-12-30 02:10:36 +0800

    init

--- web/src/main/java/com/superest/resources/hello/HelloWorld.java
+++ web/src/main/java/com/superest/resources/hello/HelloWorld.java
@@ -7,6 +7,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -15,10 +17,10 @@ public class HelloWorld {
 	
 	static Map<String, String>map = new HashMap<String, String>();
 	static{
-		map.put("hello","111111111");
-		map.put("hello","111111111");
-		map.put("hello","111111111");
-		map.put("hello","111111111");
+		map.put("hello1","111111111");
+		map.put("hello2","111111111");
+		map.put("hello3","111111111");
+		map.put("hello4","111111111");
 	}
 	
 	@Path("hello")
@@ -34,14 +36,17 @@ public class HelloWorld {
 		return map;
 	}
 	
-	@Path("userJson")
+	@Path("userXml")
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_XML)
 	public UserBean getUserJson(){
-		return new UserBean();
+		UserBean user = new UserBean();
+		user.username="sfsdf";
+		return user;
 	}
 	
 	@XmlRootElement
+	@XmlAccessorType(XmlAccessType.NONE)
 	class UserBean{
 		@XmlElement
 		private String username;
